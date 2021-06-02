3df80c0a7f7a9be98de462d03428a461e92e1ca1
williamKohin <william@dartmedia.biz>
2021-06-03 00:24:19 +0700

    fix posisiton

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -16,49 +16,51 @@
                     @if($table['data']->count())
                         @foreach ($table['data'] as $datum)
                             <tr class="border-b border-gray-200">
-                                <td class="flex items-center p-2 space-x-0">
-                                    @if(!empty($table['delete']))
-                                    <form action="{{ route($table['delete']['link'],$datum->id) }}" method="post" onsubmit="return confirm('Do you really want to delete this data?');">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit">
-                                            <div class="w-4 mr-2 text-red-800 transform hover:text-red-500 hover:scale-110">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
-                                                </svg>
-                                            </div>
-                                        </button>
-                                    </form>
-                                    @endif
-                                    @if(!empty($table['edit']))
-                                        <a href="{{ route($table['edit']['link'],$datum->id) }}">
-                                            <div class="w-4 mr-2 text-blue-800 transform hover:text-blue-500 hover:scale-110">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path>
-                                                </svg>
-                                            </div>
-                                        </a>
-                                    @endif
-                                    @if(!empty($table['confirm']))
-                                        @if($datum->{'status'} == 0)
-                                        <form action="{{ $table['confirm']['link'] }}" method="post" class="text-center">
+                                <td style="white-space: pre-line" class="flex">
+                                    <div class="flex items-center justify-center flex-1 p-2 space-x-0">
+                                        @if(!empty($table['delete']))
+                                        <form action="{{ route($table['delete']['link'],$datum->id) }}" method="post" onsubmit="return confirm('Do you really want to delete this data?');" class="flex">
                                             @csrf
-                                            <button type="submit" name="confirm" value="{{$datum->id}}" class="px-2 py-1 font-medium text-white bg-red-500 rounded-lg hover:bg-red-600">
-                                                <span>Confirm Payment</span>
+                                            @method('DELETE')
+                                            <button type="submit">
+                                                <div class="w-4 mr-2 text-red-800 transform hover:text-red-500 hover:scale-110">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+                                                    </svg>
+                                                </div>
                                             </button>
                                         </form>
                                         @endif
-                                    @endif
-                                    @if(!empty($table['detail']))
-                                        <a href="{{ route($table['detail']['link'],$datum->id) }}">
-                                            <div class="w-4 mr-2 text-gray-800 transform hover:text-gray-500 hover:scale-110">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
-                                                </svg>
-                                            </div>
-                                        </a>
-                                    @endif
+                                        @if(!empty($table['edit']))
+                                            <a href="{{ route($table['edit']['link'],$datum->id) }}">
+                                                <div class="w-4 mr-2 text-blue-800 transform hover:text-blue-500 hover:scale-110">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path>
+                                                    </svg>
+                                                </div>
+                                            </a>
+                                        @endif
+                                        @if(!empty($table['confirm']))
+                                            @if($datum->{'status'} == 0)
+                                            <form action="{{ $table['confirm']['link'] }}" method="post" class="text-center">
+                                                @csrf
+                                                <button type="submit" name="confirm" value="{{$datum->id}}" class="px-2 py-1 font-medium text-white bg-red-500 rounded-lg hover:bg-red-600">
+                                                    <span>Confirm Payment</span>
+                                                </button>
+                                            </form>
+                                            @endif
+                                        @endif
+                                        @if(!empty($table['detail']))
+                                            <a href="{{ route($table['detail']['link'],$datum->id) }}">
+                                                <div class="w-4 mr-2 text-gray-800 transform hover:text-gray-500 hover:scale-110">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path>
+                                                    </svg>
+                                                </div>
+                                            </a>
+                                        @endif
+                                    </div>
                                 </td>
                                 <td class="p-2">{{ (isset($no))?$no+=1:($no = 1 + $table['data']->perpage() * ($table['data']->currentpage() - 1)) }}</td>
                                 @foreach ($table['order'] as $name => $header)
