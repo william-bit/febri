3d19ddac0550ea02addd079ac9a8fbed803d9a3f
William Kohin <willim666.wl@gmailcom>
2022-07-07 22:22:20 +0700

    fix to febri

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,25 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/lightbox.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/lightbox.css') }}">
     <title>Admin Service</title>
 </head>
+
 <body class="overflow-hidden">
     <div class="flex h-screen bg-gray-100">
         <div class="w-64 px-4 bg-white border-r-2">
             <div class="sticky top-0 px-4 py-3">
-                <a href="{{ route('dashboard') }}" class="mb-3 text-2xl font-bold text-gray-800 uppercase">Admin Oriental</a>
+                <a href="{{ route('dashboard') }}" class="mb-3 text-2xl font-bold text-gray-800 uppercase">Admin Febri
+                    Kp</a>
                 <nav class="flex flex-col mt-8 space-y-3">
-                    <a href="{{ route('dashboard') }}" class="w-full p-2 rounded-lg font-semibold {{ request()->is('admin') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200'}}">Dashboard</a>
+                    <a href="{{ route('dashboard') }}"
+                        class="w-full p-2 rounded-lg font-semibold {{ request()->is('admin') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200' }}">Dashboard</a>
                     @auth
-                        <a href="{{ route('product') }}" class="w-full p-2 font-semibold rounded-lg {{ request()->is('admin/product') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200'}}">List Product</a>
-                        <a href="{{ route('category') }}" class="w-full p-2 font-semibold rounded-lg {{ request()->is('admin/category') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200'}}">List Category</a>
-                        <a href="{{ route('member') }}" class="w-full p-2 font-semibold rounded-lg {{ request()->is('admin/member') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200'}}">List Member</a>
-                        <a href="{{ route('transaction') }}" class="w-full p-2 font-semibold rounded-lg {{ request()->is('admin/transaction') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200'}}">Report Transaction</a>
+                        <a href="{{ route('product') }}"
+                            class="w-full p-2 font-semibold rounded-lg {{ request()->is('admin/product') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200' }}">List
+                            Product</a>
+                        <a href="{{ route('category') }}"
+                            class="w-full p-2 font-semibold rounded-lg {{ request()->is('admin/category') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200' }}">List
+                            Category</a>
+                        <a href="{{ route('member') }}"
+                            class="w-full p-2 font-semibold rounded-lg {{ request()->is('admin/member') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200' }}">List
+                            Member</a>
+                        <a href="{{ route('transaction') }}"
+                            class="w-full p-2 font-semibold rounded-lg {{ request()->is('admin/transaction') ? 'bg-gray-200 rounded-lg shadow-sm' : 'hover:bg-gray-200' }}">Report
+                            Transaction</a>
                     @endauth
                 </nav>
             </div>
@@ -30,17 +42,20 @@
                     <li class="uppercase">{{ $title }}</li>
                 </ul>
                 <ul class="flex px-3 space-x-3">
-                    @if(auth()->user())
+                    @if (auth()->user())
                         <li>
-                            <a href="{{route('home')}}" class="font-bold text-gray-300 hover:text-gray-400 text-md">Go To Home</a>
+                            <a href="{{ route('home') }}"
+                                class="font-bold text-gray-300 hover:text-gray-400 text-md">Go To Home</a>
                         </li>
                         <li>
                             <a href="" class="font-bold text-md">{{ auth()->user()->name }}</a>
                         </li>
                         <li>
-                            <form action="{{route('logout')}}" method="POST">
+                            <form action="{{ route('logout') }}" method="POST">
                                 @csrf
-                                <button class="px-4 py-1 text-sm font-semibold text-center text-red-500 rounded-3xl hover:text-red-600">Log Out</button>
+                                <button
+                                    class="px-4 py-1 text-sm font-semibold text-center text-red-500 rounded-3xl hover:text-red-600">Log
+                                    Out</button>
                             </form>
                         </li>
                     @else
@@ -48,7 +63,9 @@
                             <a class="font-semibold" href="">Guest</a>
                         </li>
                         <li>
-                            <a href="{{ route('login') }}" class="px-4 py-1 text-sm font-medium text-center text-white bg-blue-500 rounded-3xl hover:bg-blue-600">Log In</a>
+                            <a href="{{ route('login') }}"
+                                class="px-4 py-1 text-sm font-medium text-center text-white bg-blue-500 rounded-3xl hover:bg-blue-600">Log
+                                In</a>
                         </li>
                     @endif
                 </ul>
@@ -56,7 +73,8 @@
             @yield('content')
         </div>
     </div>
-    <script src="{{asset('js/lightbox.js')}}"></script>
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{ asset('js/lightbox.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
--- resources/views/pages/layout/app.blade.php
+++ resources/views/pages/layout/app.blade.php
@@ -1,69 +1,93 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/lightbox.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/lightbox.css') }}">
     <title>Alvin Wijaya Web</title>
-    <meta name="csrf-token" content="{{csrf_token()}}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
+
 <body class="flex flex-col bg-blue-100">
     <div class="sticky top-0 z-10 flex flex-col flex-1">
         <div class="flex items-center justify-between px-4 py-2 bg-blue-700">
             <div class="flex items-center space-x-6 font-bold text-blue-50">
-                <a href="{{route('home')}}" class="flex items-center text-3xl font-extrabold tracking-widest cursor-pointer hover:text-blue-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                <a href="{{ route('home') }}"
+                    class="flex items-center text-3xl font-extrabold tracking-widest cursor-pointer hover:text-blue-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-2" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                     </svg>
-                    ORIENTAL
+                    Febri
                 </a>
-                <form action="{{route('home.selection')}}" class="flex-1">
+                <form action="{{ route('home.selection') }}" class="flex-1">
                     @csrf
                     <div class="relative w-full " id="search-box">
-                        <input type="text" name="search" placeholder=" Search your item" class="p-1 text-blue-900 border-2 rounded-lg outline-none bg-blue-50 w-96">
-                        <ul class="absolute z-10 hidden w-full p-1 text-gray-600 bg-white border-b-2 border-l-2 border-r-2 rounded-lg" id='suggest-box'> </ul>
+                        <input type="text" name="search" placeholder=" Search your item"
+                            class="p-1 text-blue-900 border-2 rounded-lg outline-none bg-blue-50 w-96">
+                        <ul class="absolute z-10 hidden w-full p-1 text-gray-600 bg-white border-b-2 border-l-2 border-r-2 rounded-lg"
+                            id='suggest-box'> </ul>
                     </div>
                 </form>
             </div>
             <div class="flex items-center space-x-2">
-                @if(auth()->user())
-                    <a href="{{ route('checkout')}}" class="flex px-3 py-2 space-x-2 text-white bg-green-500 rounded-md cursor-pointer hover:bg-green-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                @if (auth()->user())
+                    <a href="{{ route('checkout') }}"
+                        class="flex px-3 py-2 space-x-2 text-white bg-green-500 rounded-md cursor-pointer hover:bg-green-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
-                        <span id="checkOutBag">{{is_array(session('productCheckout'))?count(session('productCheckout')):0}}</span>
+                        <span
+                            id="checkOutBag">{{ is_array(session('productCheckout')) ? count(session('productCheckout')) : 0 }}</span>
                         <span>
                             items in cart
                         </span>
                     </a>
                     <div class="dropdown">
-                        <button class="w-10 h-10 text-xl font-bold text-center bg-white rounded-full dropdown-button hover:bg-gray-200"> {{Str::upper(substr(auth()->user()->name,0,1))}}</button>
-                        <div class="absolute z-20 flex-col hidden w-48 p-2 mt-3 text-sm bg-white border rounded dropdown-list right-4">
-                            @if(auth()->user())
-                                @if(auth()->user()->role_id === 1)
-                                    <a href="{{route('dashboard')}}" class="px-2 py-1 text-lg rounded hover:bg-blue-200">Go to Admin Panel</a>
+                        <button
+                            class="w-10 h-10 text-xl font-bold text-center bg-white rounded-full dropdown-button hover:bg-gray-200">
+                            {{ Str::upper(substr(auth()->user()->name, 0, 1)) }}</button>
+                        <div
+                            class="absolute z-20 flex-col hidden w-48 p-2 mt-3 text-sm bg-white border rounded dropdown-list right-4">
+                            @if (auth()->user())
+                                @if (auth()->user()->role_id === 1)
+                                    <a href="{{ route('dashboard') }}"
+                                        class="px-2 py-1 text-lg rounded hover:bg-blue-200">Go to Admin Panel</a>
                                 @endif
                             @endif
-                            <a class="px-2 py-1 text-lg rounded hover:bg-blue-200" href="{{route('purchase_list')}}">Purchase list</a>
-                            <a class="px-2 py-1 text-lg rounded hover:bg-blue-200" href="{{ route('user')}}">Profile</a>
-                            <form action="{{route('logout')}}" method="post" class="px-2 py-1 rounded hover:bg-blue-200">
+                            <a class="px-2 py-1 text-lg rounded hover:bg-blue-200"
+                                href="{{ route('purchase_list') }}">Purchase list</a>
+                            <a class="px-2 py-1 text-lg rounded hover:bg-blue-200"
+                                href="{{ route('user') }}">Profile</a>
+                            <form action="{{ route('logout') }}" method="post"
+                                class="px-2 py-1 rounded hover:bg-blue-200">
                                 @csrf
                                 <button class="w-full text-left">Log Out</button>
                             </form>
                         </div>
                     </div>
                 @else
-                    <a href="{{ route('login')}}" class="flex px-3 py-2 space-x-2 text-white bg-green-500 rounded-md cursor-pointer hover:bg-green-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    <a href="{{ route('login') }}"
+                        class="flex px-3 py-2 space-x-2 text-white bg-green-500 rounded-md cursor-pointer hover:bg-green-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
-                        <span id="checkOutBag">{{is_array(session('productCheckout'))?count(session('productCheckout')):0}}</span>
+                        <span
+                            id="checkOutBag">{{ is_array(session('productCheckout')) ? count(session('productCheckout')) : 0 }}</span>
                         <span>
                             items in cart
                         </span>
                     </a>
-                    <a href={{ route('login') }} class="px-2 text-lg font-bold text-white rounded-lg cursor-pointer hover:text-gray-100">Sign In</a>
+                    <a href={{ route('login') }}
+                        class="px-2 text-lg font-bold text-white rounded-lg cursor-pointer hover:text-gray-100">Sign
+                        In</a>
                 @endif
             </div>
         </div>
@@ -78,12 +102,13 @@
     const APP_URL = '{!! URL::to('/') !!}';
 </script>
 <script>
-    function selectSuggest(element){
+    function selectSuggest(element) {
         let selectUserData = element.textContent;
         let searchBox = document.getElementById('search-box');
         searchBox.querySelector('input').value = selectUserData;
     }
 </script>
-<script src="{{asset('js/lightbox.js')}}"></script>
-<script src="{{ asset('js/app.js')}}" async></script>
+<script src="{{ asset('js/lightbox.js') }}"></script>
+<script src="{{ asset('js/app.js') }}" async></script>
+
 </html>
